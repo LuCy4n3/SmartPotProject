b9f75dd0bf9958ca957c6a6b0b2eb38960b4b840
Simonca Raul <simonca.raul@gmail.com>
2024-09-10 18:28:03 +0300

    test
    Signed-off-by: Simonca Raul <simonca.raul@gmail.com>

--- SQLserverFinale/SQLserverFinale/obj/Debug/net8.0/SQLserverFinale.AssemblyInfo.cs
+++ SQLserverFinale/SQLserverFinale/obj/Debug/net8.0/SQLserverFinale.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SQLserverFinale")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36d95b2109d74bf216708097cbab30ffca92715f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SQLserverFinale")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SQLserverFinale")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
